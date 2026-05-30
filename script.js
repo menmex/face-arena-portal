1cf99a9e2080737b602dc68e4ef0cfce591b7701
Menmex001 <idrisanderumohammed2521@gmail.com>
2026-05-30 22:27:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+import { db } from "./firebase/config.js";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
+const form = document.getElementById("contestForm");
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const fullName = document.getElementById("fullName").value;
+  const stageName = document.getElementById("stageName").value;
+  const gender = document.getElementById("gender").value;
+  const state = document.getElementById("state").value;
+  const school = document.getElementById("school").value;
+
+  try {
+    await addDoc(collection(db, "contestants"), {
+      fullName,
+      stageName,
+      gender,
+      state,
+      school,
+      createdAt: serverTimestamp()
+    });
+
+    msg.innerText = "✅ Registration successful!";
+    form.reset();
+
+  } catch (error) {
+    console.error(error);
+    msg.innerText = "❌ Error submitting form";
+  }
+});
